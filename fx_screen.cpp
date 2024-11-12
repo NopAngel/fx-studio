c9ce4ea70dd1afe4ae10cf3dcee282de570e42a4
NopAngel <angelgabrielnieto@outlook.com>
2024-11-12 12:33:57 -0400

    new config: fx code

---
+++ fx_screen.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "config.h"
+
+int main(int argc, char **argv) {
+	std::cout << "Hello World" << std::endl;
+	std::cout << "Version " << fx_screen_VERSION_MAJOR << "." << fx_screen_VERSION_MINOR << std::endl;
+	return 0;
+}
---
+++ src/core/a.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+	std::cout << "dasasdasds";
+	return 0;
+}
